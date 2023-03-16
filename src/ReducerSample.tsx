0045c5b8d4111ad508d50f5c1df50c1f491f3fff
taeryu7 <pegasy02@gmail.com>
2023-03-16 10:05:59 +0900

    0316_before_3

--- src/ReducerSample.tsx
+++ src/ReducerSample.tsx
@@ -1,3 +1,45 @@
+
+// TypeScript 와 Context API 활용하기
+import React from 'react';
+import { useSampleState, useSampleDispatch } from './SampleContext';
+
+function ReducerSample() {
+  const state = useSampleState();
+  const dispatch = useSampleDispatch();
+
+  const setCount = () => dispatch({ type: 'SET_COUNT', count: 5 }); // count 를 넣지 않으면 에러발생
+  const setText = () => dispatch({ type: 'SET_TEXT', text: 'bye' }); // text 를 넣지 않으면 에러 발생
+  const setColor = () => dispatch({ type: 'SET_COLOR', color: 'orange' }); // orange 를 넣지 않으면 에러 발생
+  const toggleGood = () => dispatch({ type: 'TOGGLE_GOOD' });
+
+  return (
+    <div>
+      <p>
+        <code>count: </code> {state.count}
+      </p>
+      <p>
+        <code>text: </code> {state.text}
+      </p>
+      <p>
+        <code>color: </code> {state.color}
+      </p>
+      <p>
+        <code>isGood: </code> {state.isGood ? 'true' : 'false'}
+      </p>
+      <div>
+        <button onClick={setCount}>SET_COUNT</button>
+        <button onClick={setText}>SET_TEXT</button>
+        <button onClick={setColor}>SET_COLOR</button>
+        <button onClick={toggleGood}>TOGGLE_GOOD</button>
+      </div>
+    </div>
+  );
+}
+
+export default ReducerSample;
+
+
+/*
 import React, { useReducer } from 'react';
 
 type Color = 'red' | 'orange' | 'yellow';
@@ -80,3 +122,4 @@ function ReducerSample() {
 }
 
 export default ReducerSample;
+*/
